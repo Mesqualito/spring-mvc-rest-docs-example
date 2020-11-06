9a7b5419cab9b3305f9c9ddf29796d01e48bb23c
Mesqualito <jochen@gebsattel.rocks>
2020-11-06 14:16:32 +0100

    Documentation Generation with Spring REST docs
    Bug-Fixes for the Documentation generation:
    - we are not getting all the documentation which we want (see errors in phase "Maven package" like "asciidoctor: WARNING - include file not found"), e.g. the new-Request (POST) snippet isn't generated etc.
    - /src/main/asciidoc/index.adoc is not functioning correctly: the *.adoc-files are overwritten by each other; the template is wrong here
    
    (Different solution strategies!)
    Here:
    - give a new folder unique to the generation-operation in 'document("identifier", ...)' - for a new folder for all newly generated '*.adoc'-snippets
    - update the 'index.adoc' accordingly
    - correcting the mustache-template 'request-fields.snippet'
    
    HINT:
    - Mustache-Template (see: https://mustache.github.io) for Spring REST docs has to be in '/test/resources/org/springframework/restdocs/templates/request-fields.snippet'
    - [maven clean,] maven package will generate '*.adoc'-files in 'target/generated-snippets/v1/beer/' along the given mustache-template-definition
    - e.g. 'rest-fields.adoc' can be customized (language, text etc.) - see Spring REST docs documentation
    - e.g. 'curl-request.adoc', 'httpie-request.adoc' etc. take the values of either Spring autoconfiguration or parameters in '@AutoConfigureRestDocs'
    
    IDEA Intellij-HINT:
    Multiple caret mode: Ctrl+Alt+Shift+J - select all occurrences of the marked text
    
    analog zu: https://www.udemy.com/course/spring-framework-5-beginner-to-guru/learn/lecture/15431832, Lektion 28-445

--- src/test/java/com/eigenbaumarkt/restdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/com/eigenbaumarkt/restdocsexample/web/controller/BeerControllerTest.java
@@ -67,7 +67,7 @@ class BeerControllerTest {
                 .param("isCold", "yes")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-get",
                         pathParameters(
                                 parameterWithName("beerId").description("UUID of desired beer to get.")
                         ),
@@ -98,7 +98,7 @@ class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(BeerDTOJson))
                 .andExpect(status().isCreated())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-post",
                         requestFields(
                                 fields.withPath("id").ignored(),
                                 fields.withPath("version").ignored(),
